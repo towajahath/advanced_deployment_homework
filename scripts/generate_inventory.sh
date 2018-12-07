7178176b1597f6f69d3c50be14348d11e79779bf
towajahath <45639436+towajahath@users.noreply.github.com>
2018-12-07 12:34:47 -0600

    updating files

--- scripts/generate_inventory.sh
+++ scripts/generate_inventory.sh
@@ -4,14 +4,12 @@ echo "Copy a new hosts file to /etc/ansible/hosts"
 cp /root/advanced_deployment_homework/hosts /etc/ansible/hosts
 
 # exports GUID line to .bashrc only once even if run multiple times
-if ! grep --quiet GUID /$HOME/.bashrc; then 
+if ! grep --quiet GUID $HOME/.bashrc; then 
    export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
 fi
 
 source $HOME/.bashrc
 
 echo "Set the current GUID to generate the inventory"
-#sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
-mv /etc/ansible/hosts /etc/ansible/hosts.bak; sed 's/GUID/'$GUID'/g' /etc/ansible/hosts.bak > /etc/ansible/hosts
-rm -rf /etc/ansible/hosts.bak
+sed -i "s/GUID/$GUID/g" /etc/ansible/hosts
 ansible all -m shell -a 'export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc'
--- scripts/nodejs_mongo_app.sh
+++ scripts/nodejs_mongo_app.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Creating project..."
-oc new-project smoke-test >/dev/null 2>&1
+oc new-project smoke-test
 echo "Creating App..."
-oc new-app nodejs-mongo-persistent >/dev/null 2>&1
+oc new-app nodejs-mongo-persistent
 echo "Waiting for complete..."
-while [ ! `oc get pod | grep Complete` ]; do
-   sleep 30
-done
+sleep 180
 test=`oc status | grep http: | awk '{print $1}'`
-echo $test
-if `curl $test >/dev/null 2>&1`; then  
-   echo "Smoketest passed"
-else 
-   echo "Smoketest failed"
-   exit 1
-fi
+curl $test
